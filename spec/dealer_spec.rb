63f3265d9c2f56d2ca47d7d771c8d9ace9670fd8
Aaron <parisi.aaron@gmail.com>
2020-06-19 10:48:49 -0700

    specs

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -3,14 +3,6 @@ require 'dealer'
 
 describe Dealer do
 
-    let(:aDeck) {double("aDeck")}
-    let(:aGame) {double("aGame")}
-
-    subject(:aDealer) {Dealer.new(aDeck, aGame)}
-
-    let(:aPlayer) {double("aPlayer")}
-    let(:aCard) {double("aCard")}
-
     describe "#play_round" do
         it "deals each player 5 cards"
         it "asks each player for initial actions"
@@ -22,27 +14,10 @@ describe Dealer do
     end
 
     describe "#deal" do
-    # so the issue is this: i have two doubles
-    # aPlayer (who needs to receive a call to receive_cards), and
-    # aDeck   (who needs to receive a call to deal_n)
-    # the problem is, each call to Dealer#deal will make both of those calls
-    # I need both doubles to be able to receive those calls
-    # even if I'm not testing that expectation in the example
-
-        before(:all) do
-            allow(aPlayer).to receive(:receive_cards)
-            allow(aDeck).to receive(:deal_n)
-        end
         it "calls the given player's #receive_cards method" do
-            expect(aPlayer).to receive(:receive_cards)
-
-            aDealer.deal(aPlayer, 5)
         end
 
         it "calls the deck's deal_n method" do
-            expect(aDeck).to receive(:deal_n).with(5)
-
-            aDealer.deal(aPlayer, 5)
         end
     end
 
