0867f43374a063c0a46bfe2c2ee8e02fe123fe09
Aaron <parisi.aaron@gmail.com>
2020-06-19 15:21:48 -0700

    Card class and spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,8 +3,13 @@ require 'rspec'
 require 'card'
 
 describe "Card" do
+    subject(:aCard) {Card.new("5", "Clubs")}
     describe "#initialize" do
-        it "sets the value"
-        it "sets the suit"
+        it "sets the value" do
+            expect(aCard.value).to eq("5")
+        end
+        it "sets the suit" do
+            expect(aCard.suit).to eq("Clubs")
+        end
     end
 end
