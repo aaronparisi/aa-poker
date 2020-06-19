ec1153bae24ae03a4ed30f82da9bcba1d22f8afb
Aaron <parisi.aaron@gmail.com>
2020-06-19 15:37:51 -0700

    added card double to Hand spec

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -5,7 +5,8 @@ require 'card'
 
 describe Hand do
     subject { described_class.new }
-    let(:card) { Card.new(10, 'spades') }
+    #let(:card) { Card.new(10, 'spades') }
+    let(:aCard) {double("card")}
 
     describe '#initialize' do
         it 'initializes an empty hand' do
@@ -15,8 +16,8 @@ describe Hand do
     
     describe '#add' do
         it 'adds a card to the hand' do
-             subject.add(card)
-             expect(subject.cards).to include(card)
+             subject.add(aCard)
+             expect(subject.cards).to include(aCard)
              
              # OR 
              # expect { subject.add(card) }.to change { subject.cards.length }.from(0).to(1)
@@ -24,9 +25,9 @@ describe Hand do
     end
     
     describe '#remove' do
-        before(:each) { subject.add(card) }
+        before(:each) { subject.add(aCard) }
         it 'removes a card from the hand' do
-            subject.remove(card)
+            subject.remove(aCard)
             expect(subject.cards).to be_empty
 
             # OR 
@@ -37,12 +38,12 @@ describe Hand do
     
     describe '#include' do
         it 'returns true if the deck includes the card' do
-            subject.add(card)
-            expect(subject).to include(card)
+            subject.add(aCard)
+            expect(subject).to include(aCard)
         end
 
         it 'returns false if the deck doesn\'t include the card' do
-            expect(subject).to_not include(card)
+            expect(subject).to_not include(aCard)
         end
     end
 end
